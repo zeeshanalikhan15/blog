4590083e87e0e36577069715685df75132dca62d
zeeshanalikhan15 <zeeshana@virtualq.com>
2024-04-30 10:05:41 +0500

    init

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  devise_for :users,
+        controllers: {
+          omniauth_callbacks: 'users/omniauth_callbacks'
+      }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  authenticated :user do
+    root 'welcome#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root to: "home#index", as: :unauthenticated_root
+  end
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
